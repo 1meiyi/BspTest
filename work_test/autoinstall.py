68669cda769860b80a965c13b4bb7e9b2e2c8c10
my <17381857304@163.com>
2025-03-04 16:17:44 +0800

    add new function1

--- work_test/autoinstall.py
+++ work_test/autoinstall.py
@@ -20,7 +20,6 @@ class Autoinstall:
         self.buckets = 'buckets/'
 
     def get_headers(self):
-
         return {
             'Content-Type': 'application/json',
             'Cookie': f"token={self.token}",
@@ -31,7 +30,6 @@ class Autoinstall:
         headers = {
             'Content-Type': 'application/json'}
         for i in range(5):
-
             try:
                 print(f'第{i + 1}次尝试获取session id')
                 res = self.requests.post(url=self.url + path, data=self.payload, headers=headers, timeout=15)
@@ -64,30 +62,28 @@ class Autoinstall:
         response = self.requests.get(f'{self.url}{self.buckets}')
         print(response.status_code)
 
-    def get_GPU_driver(self):
+    def get_gpu_driver(self):
         # timedate = datetime.date.today().strftime('%Y%m%d')
         prefix = self.enbase('release_M1000_1.2.0/')
         url = f'{self.url}{self.buckets}product-release/objects?prefix={prefix}'
         time_date = [i['name'] for i in self.requests.get(url, headers=self.get_headers()).json()['objects']]
         deb_url = f'{self.url}{self.buckets}product-release/objects?prefix={self.enbase(f'{time_date[-1]}')}'
-        res = self.requests.get(deb_url, headers=self.get_headers())
-        deb_name = [i['name'] for i in res.json()['objects'] if i['name'].endswith('.deb')]
+        deb_name = [i['name'] for i in self.requests.get(deb_url, headers=self.get_headers()).json()['objects'] if
+                    i['name'].endswith('.deb')]
         for deb in deb_name:
             if deb.endswith('.deb'):
                 print(f'wget https://oss.mthreads.com/{deb} -P ./Daily/gpu')
-                pass # 执行下载
+                pass  # 执行下载
+
     def get_kernels(self):
         # timedate = datetime.date.today().strftime('%Y%m%d')
         prefix = self.enbase(f'm1000/daily/master/aibook-6.6-ubuntu/')
         master_url = f'{self.url}{self.buckets}sw-build/objects?prefix={prefix}'
-        # res_date = self.requests.get(master_url, headers=self.get_headers())
         time_date = [i['name'] for i in self.requests.get(master_url, headers=self.get_headers()).json()['objects']]
-        # timedate = [re.search('[0-9]{8}', i['name']).group() for i in res_date.json()['objects'] if
-        #             re.search('[0-9]{8}', i['name']).group()]
         deb_prefix = self.enbase(f'{time_date[-1]}kernel-debs/')
         deb_url = f'{self.url}{self.buckets}sw-build/objects?prefix={deb_prefix}'
-        deb_res = requests.get(deb_url, headers=self.get_headers())
-        deb_name = [i['name'] for i in deb_res.json()['objects'] if i['name'].endswith('.deb')]
+        deb_name = [i['name'] for i in requests.get(deb_url, headers=self.get_headers()).json()['objects'] if
+                    i['name'].endswith('.deb')]
         for deb in deb_name:
             if deb.endswith('.deb'):
                 print(f'wget https://oss.mthreads.com/sw-build/{deb} -P ./Daily/kernel/')
@@ -113,7 +109,7 @@ if __name__ == '__main__':
     # https://oss.mthreads.com:9001/api/v1/buckets/release-ci/objects?prefix=Z3ItdW1kL3JlbGVhc2VfTTEwMDBfMS4yLjAv
     fp = Autoinstall()
     fp.get_session()
-    fp.get_GPU_driver()
+    fp.get_gpu_driver()
     fp.get_kernels()
     # fp.get_buckets()
     # fp.get_gr_umd()
